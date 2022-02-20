72f0750b121e5658c081450ec6db47c23afde5bc
quinn5moli <quinn.moli@gmail.com>
2022-02-20 15:00:28 -0800

    redirect user to home page after submitting recipe

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import { useFetch } from '../../hooks/useFetch'
 
 // styles
@@ -13,6 +14,7 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
+  const history = useHistory()
 
   const { postData, data, error } = useFetch('http://localhost:3000/recipes', "POST")
 
@@ -31,6 +33,13 @@ export default function Create() {
     setNewIngredient('')
     ingredientInput.current.focus()
   }
+
+  // redirect on submission of data
+  useEffect(() => {
+    if (data) {
+      history.push('/')
+    }
+  }, [data])
 
   return (
     <div className="create">
