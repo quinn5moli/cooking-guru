2a82e4947333274ce9fe9d74cbec0ab1d91d3872
quinn5moli <quinn.moli@gmail.com>
2022-02-20 13:50:59 -0800

    Create form developed and styled

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,12 +1,59 @@
+import { useState } from 'react'
+
 // styles
 import'./Create.css'
 
 
 
 export default function Create() {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title, method, cookingTime)
+  }
+
   return (
-    <div>
-        Create
+    <div className="create">
+        <h2 className="page-title"></h2>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Recipe title:</span>
+            <input 
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              required 
+            />
+          </label>
+
+          {/* ingredients go here */}
+
+          <label>
+            <span>Recipe method:</span>
+            <textarea 
+              onChange={(e) => setMethod(e.target.value)}
+              value={method}
+              required
+            />
+          </label>
+
+          <label>
+            <span>Cooking time (minutes):</span>
+            <input 
+              type="number"
+              onChange={(e) => setCookingTime(e.target.value)}
+              value={cookingTime}
+              required
+            />
+          </label>
+
+          <button className="btn">submit</button>
+
+        </form>
     </div>
   )
 }
