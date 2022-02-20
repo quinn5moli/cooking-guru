2a4e82c12977a56b40a212033ff6f44afe29ac30
quinn5moli <quinn.moli@gmail.com>
2022-02-20 13:18:11 -0800

    Recipe display template redone; styles added to Recipe

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,42 @@
+import { useEffect } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
+
+// hooks
+import { useFetch } from '../../hooks/useFetch'
+
 // styles
 import './Recipe.css'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = 'http://localhost:3000/recipes/' + id 
+  const { data: recipe, isPending, error } = useFetch(url)
+  const history = useHistory()
+
+  useEffect(() => {
+    if(error){
+        // redirect
+        // history.goBack()
+        setTimeout(() => {
+            history.push('/')
+        }, 2000)
+    }
+}, [error])
+
   return (
-    <div>
-        Recipe
+    <div className="recipe">
+        {isPending && <div className="loading">Loading...</div>}
+        {error && <div className="error">{error}</div>}
+        {recipe && (
+            <>
+              <h2 className="page-title">{recipe.title}</h2>
+              <p>Takes {recipe.cookTime} to make</p>
+              <ul>
+                {recipe.ingredients.map(ing => <li key ={ing}>{ing}</li> )}
+              </ul>
+              <p className="method">{recipe.method}</p>
+            </>
+        )}
     </div>
   )
 }
