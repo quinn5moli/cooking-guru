10f6c78b9e28022165031a4f065104c237ad2a6b
quinn5moli <quinn.moli@gmail.com>
2022-02-26 14:46:32 -0800

    error handling added to useTheme

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -4,5 +4,9 @@ import { ThemeContext } from '../context/ThemeContext'
 export const useTheme = () => {
     const context = useContext(ThemeContext)
 
+    if (context === undefined) {
+        throw new Error("useTheme() must be used inside a ThemeProvider")
+    }
+
     return context
 }
