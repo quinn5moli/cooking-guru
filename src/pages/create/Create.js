f3e4d0a034e47fd5ac55375e3549559f079ce81a
quinn5moli <quinn.moli@gmail.com>
2022-03-20 17:11:18 -0700

    add recipe to firestore functionality

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import { useFetch } from '../../hooks/useFetch'
+import { projectFirestore } from '../../firebase/config'
 
 // styles
 import'./Create.css'
@@ -16,11 +16,16 @@ export default function Create() {
   const ingredientInput = useRef(null)
   const history = useHistory()
 
-  const { postData, data, error } = useFetch('http://localhost:3000/recipes', "POST")
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes' })
+    const doc = { title, ingredients, method, cookingTime: cookingTime + ' minutes' }
+
+    try {
+      await projectFirestore.collection('recipes').add(doc)
+      history.push('/')
+    } catch(err) {
+      console.log(err)
+    }
   }
 
   const handleAdd = (e) => {
@@ -33,13 +38,6 @@ export default function Create() {
     setNewIngredient('')
     ingredientInput.current.focus()
   }
-
-  // redirect on submission of data
-  useEffect(() => {
-    if (data) {
-      history.push('/')
-    }
-  }, [data])
 
   return (
     <div className="create">
