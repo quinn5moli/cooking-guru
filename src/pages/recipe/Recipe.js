6ee252d2c41b2ed9851c2822960a7afab3ffdfe4
quinn5moli <quinn.moli@gmail.com>
2022-03-20 17:46:56 -0700

    update/edit recipe functionality

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -31,6 +31,12 @@ export default function Recipe() {
     })
   }, [id])
 
+  const handleClick = () => {
+    projectFirestore.collection('recipes').doc(id).update({
+      title: 'Something completely different'
+    })
+  }
+
   return (
     <div className={`recipe ${mode}`}>
         {isPending && <div className="loading">Loading...</div>}
@@ -43,6 +49,7 @@ export default function Recipe() {
                 {recipe.ingredients.map(ing => <li key ={ing}>{ing}</li> )}
               </ul>
               <p className="method">{recipe.method}</p>
+              <button onClick={handleClick}>Update</button>
             </>
         )}
     </div>
